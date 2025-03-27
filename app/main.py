a411f3078cc94d14156e0369758268867276f78b
Viktor Velychko <velviktor831@gmail.com>
2025-03-27 15:08:11 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = [Person(person_dict["name"], person_dict["age"])
+                   for person_dict
+                   in people]
+
+    for person_dict, person in zip(people, person_list):
+        wife_name = person_dict.get("wife")
+        if wife_name:
+            person.wife = Person.people[wife_name]
+        husband_name = person_dict.get("husband")
+        if husband_name:
+            person.husband = Person.people[husband_name]
+    return person_list
